2c597b37a058914a3c276abd8acd880f9bdc352e
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-12 10:29:19 -0500

    TrueCoders variables exercise

--- CsharpVariables/Program.cs
+++ CsharpVariables/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("GoodMorning! Press any key to Continue: ");
+Console.ReadLine(); 
+Console.Write("Please Enter your First Name: "); 
+string firstName = Console.ReadLine();
+Console.Write("Please Enter your Last Name: ");
+string lastName = Console.ReadLine();
+Console.Write("Please Enter your Middle Initial: ");
+char middleInitial = Convert.ToChar(Console.ReadLine());
+Console.Write("Please Enter your Age: ");
+int myAge = Convert.ToInt32(Console.ReadLine());
+Console.Write("Do you like pizza(Y/N)? ");
+string answer = Console.ReadLine().ToUpper();
+if (answer == "Y")
+{
+    bool myAnswer = true;
+    Console.Write("Wheeewwww, You may continue ");
+    Console.Write("How many pizzas could you eat? ");
+    double pizzaAnswer = Convert.ToDouble(Console.ReadLine());
+    if (pizzaAnswer >= 3)
+    {
+        Console.Write("Nicccceeeeee, That is a lot of pizza!!! ");
+        Console.Write("What is the most you would spend on a Pizza? ");
+        decimal money = Convert.ToDecimal(Console.ReadLine());
+        Console.Write($"So let me get this straight.\nYour name is {firstName} {middleInitial} {lastName} \nYou are {myAge} years old \nYou really like pizza, so much so that you could eat {pizzaAnswer} pizzas \nThe most you would spend on one is ${money} dollars. \nIs this correct(Y/N)? ");
+        Console.ReadLine(); 
+    }
+    else
+    {
+        Console.Write("You can do better than that!!!");
+        Console.ReadLine(); 
+    }
+}
+else
+{
+    bool myAnswer = false;
+    Console.Write("YOU DONT LIKE PIZZA?!?!?");
+    Console.Write("Are you sure(Y/N)? ");
+    string newAnswer = Console.ReadLine().ToUpper();
+
+    if (newAnswer == "Y")
+    {
+        Console.Write("Wow, I dont know what to say... Goodbye! ");
+        Console.ReadLine(); 
+    }
+    else
+    {
+        Console.Write("I still don't know if I can trust you... Goodbye! ");
+        Console.ReadLine(); 
+    }
+}
 
